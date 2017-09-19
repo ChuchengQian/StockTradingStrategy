499c31f29be3c09d22b21beeb061d1f1bc71cdd0
Chucheng Qian <chuchengqian@ChuchengmatoMacBook-Pro.local>
2017-09-20 09:04:03 +1000

    tesing7

--- Sources/Order.hs
+++ Sources/Order.hs
@@ -2,8 +2,6 @@ module Order where
 
 import Types
 
-
-
 -- Change this implementation to your own non-trivial trading strategy.
 -- Do not modify the type signature of the function.
 --
@@ -28,29 +26,30 @@ import Types
 --[]
 
 
-
+--makeOrders obtains a list of buying and selling order
 makeOrders :: Portfolio -> [StockHistory] -> [Order]
 makeOrders (cash, holds) history =
     case (belowAverage holds history) of
         x:xs -> [Order (fst x) (-(snd x))] ++ makeOrders (cash, xs) history
         []
-         |(aboveAverage history 1.01)==[]->[]
-         |otherwise -> makeOrders2 (cash, holds) history (aboveAverage history 1.01)
+         |(aboveAverage history 1.03)==[]->[]
+         |otherwise -> makeOrders2 (cash, holds) history (aboveAverage history 1.03)
 
-
+--makeOrders2 obtains a list of buying orders by recursion and it supports the makeOrders function
 makeOrders2 :: Portfolio -> [StockHistory] ->[StockHistory]-> [Order]
 makeOrders2 (cash, holds) history a = case a of
          []->[]
          c:cs ->[Order (fst c) (quantity cash history)] ++ makeOrders2 (cash, holds) history cs
 
 
-
+--average obtains the past 5-day average price of one stock
 average :: Stock -> [StockHistory] -> Price
 average s histories = (sum (take 5 (tail (snd getStock)))) / 5
     where
         getStock = head $ filter (\x -> fst x == s) histories
 
-
+--aboveAverage checks if the prices of stocks shows an upward trend
+-- and picks out the first 5 stocks which have increasing prices
 aboveAverage :: [StockHistory] -> Double -> [StockHistory]
 aboveAverage stocks rate
     | stocks == [] = []
@@ -59,7 +58,8 @@ aboveAverage stocks rate
     where
         getdays= length(snd (head stocks))
 
-
+--belowAverage checks if the prices of stocks in holding shows an downward trend
+-- and picks out all of stocks which have decreasing prices
 belowAverage :: Holdings -> [StockHistory] -> Holdings
 belowAverage holds stocks
     | stocks ==[]= []
@@ -69,17 +69,17 @@ belowAverage holds stocks
     where
         getdays= length(snd (head stocks))
 
-
+--quantity calculates the amount of a stock one is going to buy
 quantity :: Cash -> [StockHistory] -> Quantity
-quantity cash stocks = floor(cash / (sum (getPriceSum (aboveAverage stocks 1.01))))
+quantity cash stocks = floor(cash / (sum (getPriceSum (aboveAverage stocks 1.03))))
 
+--getPriceSum obtains the list of the current price of the stocks one want to buy
 getPriceSum:: [StockHistory] -> [Price]
-getPriceSum history = case (aboveAverage history 1.01) of
+getPriceSum history = case (aboveAverage history 1.03) of
             []->[]
             x:xs-> [getStockPrice (fst x) history]++getPriceSum xs
 
-
-
+--getStockPrice calculate the stock price
 getStockPrice :: Stock -> [StockHistory] -> Price
 getStockPrice s histories = head $ snd getStock
     where
